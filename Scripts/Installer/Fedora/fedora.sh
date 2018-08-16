f1f424f3e2782dd6c3f02b27c4af732d835d9311
cat999 <33449553+exalab999@users.noreply.github.com>
2018-08-16 10:23:30 +0800

    Update fedora.sh

--- Scripts/Installer/Fedora/fedora.sh
+++ Scripts/Installer/Fedora/fedora.sh
@@ -24,11 +24,11 @@ if [ "$first" != 1 ];then
 	mkdir -p "$folder"
 	cd "$folder"
 	echo "Decompressing Rootfs, please be patient."
-	proot --link2symlink tar -xJf ${cur}/${tarball} --exclude='dev' ||:
+	tar -xJf ${cur}/${tarball} --strip-components=1 --exclude json --exclude VERSION && tar -xf layer.tar||:
 	echo "Setting up name server"
 	echo "127.0.0.1 localhost" > /etc/hosts
-    echo "nameserver 8.8.8.8" > /etc/resolv.conf
-    echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+        echo "nameserver 8.8.8.8" > /etc/resolv.conf
+        echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 	cd "$cur"
 fi
 mkdir -p binds
