ee1489d39a83ba6392433d987a952001657949a0
exalab999 <exalabdevelopers@gmail.com>
2018-08-17 18:59:03 +0800

    Update Script

--- Scripts/GUI/Apt/gui-apt.sh
+++ Scripts/GUI/Apt/gui-apt.sh
@@ -2,7 +2,7 @@
 
 #Get the component
 apt-get update
-apt-get install xterm tightvncserver wget
+apt-get install xterm tightvncserver -y
 
 #Setup the necessary files
 mkdir ~/.vnc
@@ -11,12 +11,20 @@ wget https://raw.githubusercontent.com/EXALAB/LinuxOnAndroid/master/Scripts/GUI/
 chmod +x ~/.vnc/xstartup
 chmod +x /etc/init.d/vncserver
 
+echo " "
 echo "You can now start vncserver by running /etc/init.d/vncserver start"
+echo " "
 echo "It will ask you to enter a password when first time starting it."
+echo " "
 echo "The VNC Server will be started at 127.0.0.1:5901"
+echo " "
 echo "Youc can connect to this address with a VNC Viewer you prefer"
+echo " "
 echo "Connect to this address will open a window with xterm"
+echo " "
 echo "You can run any GUI program in xterm, and close them with CTRl+C"
+echo " "
 echo "Hacker Keyboard is recommended if you want to use the GUI, it could be install from Play Store"
+echo " "
 
 /etc/init.d/vncserver start
