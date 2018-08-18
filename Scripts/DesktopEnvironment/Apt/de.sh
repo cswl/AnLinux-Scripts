41a094abe0fe41410e2e267f74234508286680d5
exalab999 <exalabdevelopers@gmail.com>
2018-08-18 20:53:55 +0800

    Update Scripts

--- Scripts/DesktopEnvironment/Apt/de.sh
+++ Scripts/DesktopEnvironment/Apt/de.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+#Get the necessary components
+apt-get update
+apt-get install lxde tightvncserver -y
+
 #Setup the necessary files
 mkdir ~/.vnc
-wget https://raw.githubusercontent.com/EXALAB/LinuxOnAndroid/master/Scripts/GUI/Apt/xstartup -P ~/.vnc/
-wget https://raw.githubusercontent.com/EXALAB/LinuxOnAndroid/master/Scripts/GUI/Apt/vncserver -P /etc/init.d/
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/xstartup -P ~/.vnc/
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/vncserver -P /etc/init.d/
 chmod +x ~/.vnc/xstartup
 chmod +x /etc/init.d/vncserver
 
@@ -16,11 +20,7 @@ echo "The VNC Server will be started at 127.0.0.1:5901"
 echo " "
 echo "Youc can connect to this address with a VNC Viewer you prefer"
 echo " "
-echo "Connect to this address will open a window with xterm"
-echo " "
-echo "You can run any GUI program in xterm, and close them with CTRl+C"
-echo " "
-echo "Hacker Keyboard is recommended if you want to use the GUI, it could be install from Play Store"
+echo "Connect to this address will open a window with full LXDE Desktop Environment"
 echo " "
 echo " "
 echo " "
