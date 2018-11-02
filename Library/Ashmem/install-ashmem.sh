0d469b3b0fd73ac2b8327e297e7692666ff8c10d
exalab999 <exalabdevelopers@gmail.com>
2018-11-02 20:40:05 +0800

    Update Library/Ashmem/install-ashmem.sh

--- Library/Ashmem/install-ashmem.sh
+++ Library/Ashmem/install-ashmem.sh
@@ -1,4 +1,1 @@
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Library/Ashmem/libandroid-shmem-gnueabihf.so -P /usr/local/lib
-wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Library/Ashmem/ld.so.preload -P /etc
-
-export LD_PRELOAD=/usr/local/lib/libandroid-shmem-gnueabihf.so
