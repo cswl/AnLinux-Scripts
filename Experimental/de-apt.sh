f03ec936cf1f0ec8b1a81e3b53e1cc88db1ec25e
exalab999 <exalabdevelopers@gmail.com>
2018-11-02 11:58:54 +0800

    Final Update

--- Experimental/de-apt.sh
+++ Experimental/de-apt.sh
@@ -3,12 +3,14 @@
 #Get the necessary components
 apt-get update
 apt-mark hold systemd*
-apt-get install xfce4 tightvncserver --no-install-recommends -y
+apt-get install xfce4 gnome-terminal tightvncserver --no-install-recommends -y
 
 #Setup the necessary files
 mkdir ~/.vnc
 wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Experimental/xstartup -P ~/.vnc/
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Experimental/vncserver-start -P /usr/bin/
 chmod +x ~/.vnc/xstartup
+chmod +x /usr/bin/vncserver-start
 
 echo " "
 echo "You can now start vncserver by running /etc/init.d/vncserver start"
@@ -27,5 +29,8 @@ echo "Starting VNC Server"
 echo " "
 echo " "
 echo " "
+
+echo "export DISPLAY=":1" >> /etc/profile
+source /etc/profile
 
 vncserver
