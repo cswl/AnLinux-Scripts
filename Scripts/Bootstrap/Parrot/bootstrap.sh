873a80e5860469ce1294a2730f705c028f6f71bb
exalab999 <exalabdevelopers@gmail.com>
2018-11-20 00:23:30 +0800

    Update to include Parrot Security OS

---
+++ Scripts/Bootstrap/Parrot/bootstrap.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#Bootstrap the system
+rm -rf $2
+mkdir $2
+if [ "$1" = "i386" ] || [ "$1" = "amd64" ] ; then
+  debootstrap --no-check-gpg --arch=$1 --variant=minbase --include=systemd,libsystemd0 stable $2 http://deb.parrotsec.org/parrot
+else
+  qemu-debootstrap --no-check-gpg --arch=$1 --variant=minbase --include=systemd,libsystemd0 stable $2 http://deb.parrotsec.org/parrot
+fi
+
+#Reduce size
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true \
+ LC_ALL=C LANGUAGE=C LANG=C chroot $2 apt-get clean
+
+#Setup DNS
+echo "127.0.0.1 localhost" > $2/etc/hosts
+echo "nameserver 8.8.8.8" > $2/etc/resolv.conf
+echo "nameserver 8.8.4.4" >> $3/etc/resolv.conf
+
+#sources.list setup
+rm $2/etc/apt/sources.list
+echo "deb http://deb.parrotsec.org/parrot stable main contrib non-free" >> $2/etc/apt/sources.list
+echo "deb-src http://deb.parrotsec.org/parrot stable main contrib non-free" >> $2/etc/apt/sources.list
+#Import the gpg key, this is only required in Parrot Security OS
+wget http://deb.parrotsec.org/parrot/misc/archive.gpg -O $2/etc/apt/trusted.gpg.d/parrot-archive-key.asc
+
+#tar the rootfs
+cd $2
+rm -rf ../parrot-rootfs-$1.tar.gz
+rm -rf dev/*
+GZIP=-9 tar -cvzf ../parrot-rootfs-$1.tar.gz ./*
