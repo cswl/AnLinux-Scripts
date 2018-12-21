dc1575f01dd6b20ca33049063f1439b2d4f98bd0
exalab999 <exalabdevelopers@gmail.com>
2018-12-21 14:36:43 +0800

    exclude /dev

--- Experimental/nethunter.sh
+++ Experimental/nethunter.sh
@@ -30,7 +30,7 @@ if [ "$first" != 1 ];then
 	mkdir -p "$folder"
 	cd "$folder"
 	echo "Decompressing Rootfs, please be patient."
-	proot --link2symlink tar -xJf ${cur}/${tarball}||:
+	proot --link2symlink tar -xJf ${cur}/${tarball} --exclude='dev'||:
 	cd "$cur"
 fi
 mkdir -p nethunter-binds
