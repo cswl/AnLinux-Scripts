66517973f74bb56c26493bdefcacba184060e2c1
exalab999 <exalabdevelopers@gmail.com>
2019-03-02 18:36:30 +0800

    Include BackBox

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ To open an issue, please visit [here](https://github.com/EXALAB/AnLinux-App/issu
 
 ## Bootstraping System
 
-Note: Only [Ubuntu](https://www.ubuntu.com/), [Debian](https://www.debian.org/), [Kali](https://www.kali.org/), [Parrot Security OS](https://www.parrotsec.org/) are bootstrap using the script, others are official image without modification.
+Note: Only [Ubuntu](https://www.ubuntu.com/), [Debian](https://www.debian.org/), [Kali](https://www.kali.org/), [Parrot Security OS](https://www.parrotsec.org/), [BackBox](https://www.backbox.org) are bootstrap using the script, others are official image without modification.
 
 Script located at Scripts/Bootstrap are used to bootstrap the system, to bootstrap a system, simply run:
 
---
+++ Scripts/Bootstrap/BackBox/bootstrap.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+#Bootstrap the system
+rm -rf $2
+mkdir $2
+if [ "$1" = "i386" ] || [ "$1" = "amd64" ] ; then
+  debootstrap --no-check-gpg --arch=$1 --variant=minbase --include=systemd,libsystemd0,wget,ca-certificates xenial $2 http://archive.ubuntu.com/ubuntu
+else  
+  qemu-debootstrap --no-check-gpg --arch=$1 --variant=minbase --include=systemd,libsystemd0,wget,ca-certificates xenial $2 http://ports.ubuntu.com/ubuntu-ports
+fi
+
+#Reduce size
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true \
+ LC_ALL=C LANGUAGE=C LANG=C chroot $3 apt-get clean
+
+#Setup DNS
+echo "127.0.0.1 localhost" > $2/etc/hosts
+echo "nameserver 8.8.8.8" > $2/etc/resolv.conf
+echo "nameserver 8.8.4.4" >> $3/etc/resolv.conf
+
+#sources.list setup
+rm $2/etc/apt/sources.list
+if [ "$1" = "i386" ] || [ "$1" = "amd64" ] ; then
+  echo "deb http://archive.ubuntu.com/ubuntu xenial main restricted universe multiverse" >> $2/etc/apt/sources.list
+  echo "deb-src http://archive.ubuntu.com/ubuntu xenial main restricted universe multiverse" >> $2/etc/apt/sources.list
+  echo "" >> $2/etc/apt/sources.list
+  echo "deb http://ppa.launchpad.net/backbox/five/ubuntu xenial main" >> $2/etc/apt/sources.list
+  echo "deb-src http://ppa.launchpad.net/backbox/five/ubuntu xenial main" >> $2/etc/apt/sources.list
+else  
+  echo "deb http://ports.ubuntu.com/ubuntu-ports xenial main restricted universe multiverse" >> $2/etc/apt/sources.list
+  echo "deb-src http://ports.ubuntu.com/ubuntu-ports xenial main restricted universe multiverse" >> $2/etc/apt/sources.list
+  echo "" >> $2/etc/apt/sources.list
+  echo "deb http://ppa.launchpad.net/backbox/five/ubuntu xenial main" >> $2/etc/apt/sources.list
+  echo "deb-src http://ppa.launchpad.net/backbox/five/ubuntu xenial main" >> $2/etc/apt/sources.list
+fi
+
+#tar the rootfs
+cd $2
+rm -rf ../backbox-rootfs-$1.tar.gz
+rm -rf dev/*
+GZIP=-9 tar -cvzf ../backbox-rootfs-$1.tar.gz ./*
