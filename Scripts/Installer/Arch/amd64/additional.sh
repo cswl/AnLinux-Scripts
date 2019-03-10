40e05201b4950cb5355013bede8d446794adbc2f
exalab999 <exalabdevelopers@gmail.com>
2019-03-10 18:41:27 +0800

    Update Arch Linux

--- Scripts/Installer/Arch/amd64/additional.sh
+++ Scripts/Installer/Arch/amd64/additional.sh
@@ -4,3 +4,6 @@ mv resolv.conf /etc
 pacman-key --init
 echo "disable-scdaemon" > /etc/pacman.d/gnupg/gpg-agent.conf
 pacman-key --populate archlinux
+
+echo "Changing some permissions, please be patient"
+chmod 755 -R /bin /home /mnt /run /srv /tmp /var /boot /etc /lin /opt /root /sbin /sys /usr
--- Scripts/Installer/Arch/armhf/additional.sh
+++ Scripts/Installer/Arch/armhf/additional.sh
@@ -4,3 +4,6 @@ mv resolv.conf /etc
 pacman-key --init
 echo "disable-scdaemon" > /etc/pacman.d/gnupg/gpg-agent.conf
 pacman-key --populate archlinuxarm
+
+echo "Changing some permissions, please be patient"
+chmod 755 -R /bin /home /mnt /run /srv /tmp /var /boot /etc /lin /opt /root /sbin /sys /usr
