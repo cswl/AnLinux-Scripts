2eedcc7f09b5ddd384f4274f4a917bb0cf965bd5
exalab999 <exalabdevelopers@gmail.com>
2019-05-17 16:22:34 +0800

    Big rootfs update

--- Scripts/Bootstrap/Parrot/bootstrap.sh
+++ Scripts/Bootstrap/Parrot/bootstrap.sh
@@ -4,9 +4,9 @@
 rm -rf $2
 mkdir $2
 if [ "$1" = "i386" ] || [ "$1" = "amd64" ] ; then
-  debootstrap --no-check-gpg --arch=$1 --variant=minbase --include=systemd,libsystemd0,wget,ca-certificates stable $1 http://ba.mirror.garr.it/mirrors/parrot
+  debootstrap --no-check-gpg --arch=$1 --variant=minbase --include=busybox,systemd,libsystemd0,wget,ca-certificates,neofetch stable $1 http://ba.mirror.garr.it/mirrors/parrot
 else
-  qemu-debootstrap --no-check-gpg --arch=$1 --variant=minbase --include=systemd,libsystemd0,wget,ca-certificates stable $1 http://ba.mirror.garr.it/mirrors/parrot
+  qemu-debootstrap --no-check-gpg --arch=$1 --variant=minbase --include=busybox,systemd,libsystemd0,wget,ca-certificates,neofetch stable $1 http://ba.mirror.garr.it/mirrors/parrot
 fi
 
 #Reduce size
