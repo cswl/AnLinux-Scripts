f2ff723843edb40c38ea54f8f50ec7f899e76a3b
exalab999 <exalabdevelopers@gmail.com>
2019-05-22 23:34:53 +0800

    Update to DE Scripts

--- Scripts/DesktopEnvironment/Apt/Xfce4/de-apt-xfce4.sh
+++ Scripts/DesktopEnvironment/Apt/Xfce4/de-apt-xfce4.sh
@@ -3,6 +3,9 @@
 #Get the necessary components
 apt-get update
 apt-get install xfce4 xfce4-terminal tightvncserver -y
+apt-get remove udisks2 -y
+apt-get install xfe
+apt-get clean
 
 #Setup the necessary files
 mkdir ~/.vnc
